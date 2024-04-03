703d5ce6ca3b5251e92402c219f0da61211b3d8a
Guido Pérez Moro <90873217+GuidoPerezMoro@users.noreply.github.com>
2024-04-02 22:14:03 -0300

    Base de datos y proyecto Spring creados

---
+++ src/main/java/lab4/tp2/Tp2Application.java
@@ -0,0 +1,14 @@
+package lab4.tp2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp2Application.class, args);
+		System.out.println("---------------------------ESTOY FUNCIONANDO---------------------------");
+	}
+
+}
