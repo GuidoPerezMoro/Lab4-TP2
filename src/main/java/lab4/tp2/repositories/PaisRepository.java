9b2fcfbb8a913997d5a0cfc17cea1192cf73a9f0
Guido Pérez Moro <90873217+GuidoPerezMoro@users.noreply.github.com>
2024-04-04 19:24:45 -0300

    Borrado total. Empezando de 0

--- src/main/java/lab4/tp2/repositories/PaisRepository.java
+++
@@ -1,8 +0,0 @@
-package lab4.tp2.repositories;
-
-import lab4.tp2.entities.Pais;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PaisRepository extends BaseRepository<Pais,Integer> {
-}
--- src/main/java/lab4/tp2/services/PaisService.java
+++
@@ -1,6 +0,0 @@
-package lab4.tp2.services;
-
-import lab4.tp2.entities.Pais;
-
-public interface PaisService extends BaseService<Pais, Integer> {
-}
